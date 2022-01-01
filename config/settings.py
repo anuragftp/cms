bc338405b59d80a4365d20292dde278d9912ddad
anurag <sibuftp@gmail.com>
2022-01-01 19:31:25 +0530

    final version 2

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(9m8^)2!@dxyxst602*o*k-$+w4p*l$b8l%ydkd83gwbx9@2ls'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
