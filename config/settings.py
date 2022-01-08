afa534d03d7164f08497e31e1bc855d764292723
anurag <sibuftp@gmail.com>
2022-01-08 14:34:17 +0530

    version6

--- config/settings.py
+++ config/settings.py
@@ -89,16 +89,7 @@ DATABASES = {
     }
 }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'cms',
-#         'HOST': '127.0.0.1',
-#         'PORT': '3306',
-#         'USER': 'root',
-#         'PASSWORD': 'Anurag@123',
-#     }
-# }
+
 
 
 
